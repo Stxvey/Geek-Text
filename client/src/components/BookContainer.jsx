3301f1a9370b78829d9154618ff3b4531ba55e54
PatPerez4 <60159676+PatPerez4@users.noreply.github.com>
2020-04-10 21:08:24 -0400

    initial browse commit

--- client/src/components/BookContainer.jsx
+++ client/src/components/BookContainer.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import {Container, Form, Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Container, Form, Button, Dropdown, DropdownMenu, DropdownToggle } from 'react-bootstrap'
 import BookCard from './BookCard'
 
 function BookContainer() {
@@ -8,16 +8,47 @@ function BookContainer() {
     const [bookList, setBookList] = useState([])
 
 
-    function searchForBooks(e){
+    function searchForBooks(e) {
         //TODO: check to make sure search isn't empty
-        fetch('/books/all/').then(res => {return res.json()}).then(data => getTenBooks(data))
+        fetch('/books/all/').then(res => { return res.json() }).then(data => getTenBooks(data))
     }
     useEffect(() => {
-        searchForBooks() 
-     }, [])
-    function getTenBooks(data){
+        searchForBooks()
+    }, [])
+    /*function browseBooks(j){
+        if ()
+    }
+    function sortBooks(i){
+        if (i == 'genre'){
+
+        }
+        else if (i == 'title'){
+
+        }
+        else if (i == 'author'){
+            
+        }
+        else if (i == 'rating'){
+            
+        }
+        else if (i == 'release'){
+            
+        }
+        else if (i == 'price'){
+            
+        }
+        else if (i == 'top'){
+            
+        }
+    }*/
+    function getTenBooks(data) {
         var res = []
         res = data.sort(() => Math.random() - Math.random()).slice(0, 10)
+        setBookList(res)
+    }
+    function getTwentyBooks(data) {
+        var res = []
+        res = data.sort(() => Math.random() - Math.random()).slice(0, 20)
         setBookList(res)
     }
     const gridStyle = {
@@ -27,15 +58,53 @@ function BookContainer() {
         gridColumnGap: '10px',
         gridRowGap: '15px'
     }
-    return(
+    return (
         <>
             <Form inline>
-                <Form.Control type="text" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)}/>
-                <Button variant="outline-success" onClick={(e) => {searchForBooks(e)}}>Search</Button>
+                <Form.Control type="text" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} />
+                <Button variant="outline-success" onClick={(e) => { searchForBooks(e) }}>Search</Button>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Pick a Genre
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1" /*onClick={sortBooks('genre')}*/ >Genre</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2" /*onClick={sortBooks('title')}*/ >Title(A-Z)</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3" /*onClick={sortBooks('author')}*/ >Author(A-Z)</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4" /*onClick={sortBooks('rating')}*/ >Rating</Dropdown.Item>
+                        <Dropdown.Item href="#/action-5" /*onClick={sortBooks('release')}*/ >Release Date</Dropdown.Item>
+                        <Dropdown.Item href="#/action-6" /*onClick={sortBooks('price')}*/ >Price</Dropdown.Item>
+                        <Dropdown.Item href="#/action-7" /*onClick={sortBooks('top')}*/ >Top Sellers</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Sort By
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-2" /*onClick={sortBooks('title')}*/ >Title(A-Z)</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3" /*onClick={sortBooks('author')}*/ >Author(A-Z)</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4" /*onClick={sortBooks('rating')}*/ >Rating</Dropdown.Item>
+                        <Dropdown.Item href="#/action-5" /*onClick={sortBooks('release')}*/ >Release Date</Dropdown.Item>
+                        <Dropdown.Item href="#/action-6" /*onClick={sortBooks('price')}*/ >Price</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Display Count
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1" /*onClick={sortBooks('title')}*/ >10</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2" /*onClick={sortBooks('author')}*/ >20</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </Form>
             <Container style={gridStyle}>
                 {bookList.map((book) => (
-                    <BookCard {...book} key={book.id}/>
+                    <BookCard {...book} key={book.id} />
                 ))}
             </Container>
         </>
