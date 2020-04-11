3be6a00a78a1517ae5458085fb91a367b4a96644
Steve Patterson <Spatt030@fiu.edu>
2020-04-10 20:07:49 -0400

    Fixed something wrong with the profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,10 +4,13 @@ import {Button, Col, Form, Table} from 'react-bootstrap'
 
 function Profile(props) {
     const [addresses, setAddresses] = useState([])
+    const [cards, setCards] = useState([])
     const [user, setUser] = useState({})
     const [addAddressFormActive, setAddAddressFormActive] = useState(false)
+    const [addCardFormActive, setAddCardFormActive] = useState(false)
     useEffect(() => {
         getAddresses()
+        getCards()
         setUser(props.location.state.user)
     }, [])
 
@@ -74,10 +77,86 @@ function Profile(props) {
             return
         }
     }
+    function showAddCards(){
+        const newCard = {}
+        function handleNameChange(e){
+            newCard.full_name = e.target.value
+        }
+        function handleNumberChange(e){
+            newCard.cc_num = e.target.value
+        }
+        function handleExpirationChange(e){
+            newCard.expirationDate = e.target.value
+        }
+        function handleSecurityChange(e){
+            newCard.security_num = e.target.value
+        }
+        function handleZipChange(e){
+            newCard.zip = e.target.value
+        }
+        function handleSubmit(e){
+            e.preventDefault()
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(newCard)
+            }
+            console.log(newCard)
+            fetch('/user/addCard', requestOptions)
+            .then(res => {
+                if (res.status === 200){
+                    setAddCardFormActive(false)
+                    const newData = [...cards]
+                    newData.push(newCard)
+                    setCards(newData)
+                }
+            })
+            .catch(e => console.log(e))
+        }
+        if (addCardFormActive){
+            return(
+                <Form>
+                <Form.Group controlId="fullName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control placeholder="Bob Smith" onChange={(e) => handleNameChange(e)}></Form.Control>
+                </Form.Group>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formCCNum">
+                        <Form.Label>Credit Card Number</Form.Label>
+                        <Form.Control placeholder="1234 5678 9012 0128" onChange={(e) => handleNumberChange(e)}/>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formEXP">
+                        <Form.Label>Exp Date</Form.Label>
+                        <Form.Control onChange={(e) => handleExpirationChange(e)}/>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formCVV">
+                        <Form.Label>CVV</Form.Label>
+                        <Form.Control onChange={(e) => handleSecurityChange(e)}/>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formZip">
+                        <Form.Label>ZIP</Form.Label>
+                        <Form.Control onChange={(e) => handleZipChange(e)}/>
+                    </Form.Group>
+                </Form.Row>
+                <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e)}>
+                    Add Card
+                </Button>
+            </Form>
+            )
+        } else {
+            return
+        }
+    }
     function getAddresses(){
         fetch('/user/addresses')
         .then(res => {return res.json()})
         .then(data => setAddresses(data))
+    }
+    function getCards(){
+        fetch('/user/cards')
+        .then(res => {return res.json()})
+        .then(data => {console.log(data) 
+        setCards(data)})
     }
     return (
         <div className="container">
@@ -104,7 +183,7 @@ function Profile(props) {
                     {addresses.map((address, index) => {
                         return(
                             <tr>
-                                <td>{index}</td>
+                                <td>{index + 1}</td>
                                 <td>{address.streetAddress}</td>
                                 <td>{address.city}</td>
                                 <td>{address.state}</td>
@@ -115,6 +194,31 @@ function Profile(props) {
                 </tbody>
             </Table>
             {showAddAddress()}
+            <h1>Credit Cards</h1>
+            <i class="fas fa-plus" onClick={() => setAddCardFormActive(true)}></i> Add Card
+            <Table>
+                <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Full Name</th>
+                            <th>Credit Card Number</th>
+                            <th>EXP</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cards.map((card, index) => {
+                            return(
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{card.fullName}</td>
+                                    <td>{card.cc_num}</td>
+                                    <td>{card.expirationDate}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+            </Table>
+            {showAddCards()}
         </div>
     )
 }
